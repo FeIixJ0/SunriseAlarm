944be89667f58ee5302bb81528e3787ddb0555be
Felielix <mjoo0001@student.monash.edu>
2018-07-07 15:44:50 +1000

    added the code for playing&pausing sound

---
+++ soundPlayCode/test.js
@@ -0,0 +1,13 @@
+//test.js
+
+
+let outputAreaRef = document.getElementById("outputArea");
+let output ="";
+let aFile = new Audio('pom.mp3')
+aFile.volume = 0.3;
+aFile.play();
+
+outputAreaRef.innerHTML = output;
+
+
+
